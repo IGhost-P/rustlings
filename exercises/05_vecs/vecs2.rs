6ce8df4e5d691af9a534092264a82c1eea88ac56
sangwooyang <dndb3599@gmail.com>
2024-06-16 01:26:39 +0900

    week2: 완료

--- exercises/05_vecs/vecs2.rs
+++ exercises/05_vecs/vecs2.rs
@@ -7,13 +7,13 @@
 //
 // Execute `rustlings hint vecs2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
 fn vec_loop(mut v: Vec<i32>) -> Vec<i32> {
     for element in v.iter_mut() {
         // TODO: Fill this up so that each element in the Vec `v` is
         // multiplied by 2.
-        ???
+
+        *element = *element * 2;
     }
 
     // At this point, `v` should be equal to [4, 8, 12, 16, 20].
@@ -24,7 +24,8 @@ fn vec_map(v: &Vec<i32>) -> Vec<i32> {
     v.iter().map(|element| {
         // TODO: Do the same thing as above - but instead of mutating the
         // Vec, you can just return the new number!
-        ???
+
+        element * 2
     }).collect()
 }
 
@@ -37,6 +38,7 @@ mod tests {
         let v: Vec<i32> = (1..).filter(|x| x % 2 == 0).take(5).collect();
         let ans = vec_loop(v.clone());
 
+        // 2, 4, 6, 8, 10
         assert_eq!(ans, v.iter().map(|x| x * 2).collect::<Vec<i32>>());
     }
 
@@ -45,6 +47,7 @@ mod tests {
         let v: Vec<i32> = (1..).filter(|x| x % 2 == 0).take(5).collect();
         let ans = vec_map(&v);
 
+        // 2, 4, 6, 8, 10
         assert_eq!(ans, v.iter().map(|x| x * 2).collect::<Vec<i32>>());
     }
 }
